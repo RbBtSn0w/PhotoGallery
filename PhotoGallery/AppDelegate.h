d97f6c13f9d0a28f89fc7290a60c7811a97040ea
rbbtsn0w <hamiltonsnow@gmail.com>
2015-11-15 12:02:55 +0800

    Initial Commit

---
+++ PhotoGallery/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PhotoGallery
+//
+//  Created by Snow.y.wu on 11/15/15.
+//  Copyright © 2015 rbbtsn0w. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PhotoGallery/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PhotoGallery
+//
+//  Created by Snow.y.wu on 11/15/15.
+//  Copyright © 2015 rbbtsn0w. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
